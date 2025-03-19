07a598ddb79516a35b8e4aa6dcd3a9f0a0fb101e
shuflovic <pavel.skuta@gmail.com>
2025-03-19 20:57:56 +0800

    Update message.js

--- message.js
+++ message.js
@@ -1,7 +1,5 @@
 export function checkpointMessage(airport) {
-
     const messageBody = "stastlivo sme dorazili";
-   
     const whatsappLink = `https://wa.me/+421917809193?text=${encodeURIComponent(messageBody)}`;
     window.open(whatsappLink, '_blank'); // Open in a new tab
 }
