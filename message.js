49adaf7a006881b9b59756f3cb14a6ce4a7be189
shuflovic <pavel.skuta@gmail.com>
2025-03-25 20:15:28 +0800

    Update message.js

--- message.js
+++ message.js
@@ -1,6 +1,6 @@
 export function checkpointMessage(messageBody) {
     const whatsappLink = `https://wa.me/+421917809193?text=${encodeURIComponent(messageBody)}`;
-    console.log(titleText);
+    console.log(`WhatsApp Link: ${whatsappLink}`); // Logs the link for debugging
     window.open(whatsappLink, '_blank'); // Open in a new tab
 }
 
