df944c3a591179e2315a9c302264ecda99ac15ef
shuflovic <pavel.skuta@gmail.com>
2025-03-25 19:53:15 +0800

    Update message.js

--- message.js
+++ message.js
@@ -1,5 +1,6 @@
 export function checkpointMessage(messageBody) {
     const whatsappLink = `https://wa.me/+421917809193?text=${encodeURIComponent(messageBody)}`;
+    console.log(titleText);
     window.open(whatsappLink, '_blank'); // Open in a new tab
 }
 
