8b83ffabefbe3f7ddae8bdf6b669b1975009e55c
shuflovic <pavel.skuta@gmail.com>
2025-04-17 15:17:21 +0200

    Update new-trips.js

--- new-trips.js
+++ new-trips.js
@@ -12,7 +12,7 @@ function loadTrips() {
     trips.forEach((trip, index) => {
         // Create a new button for each trip
         const tripButton = document.createElement('button');
-        tripButton.className = 'blink-button';
+        ripButton.className = 'blink-button';
         
         // Create a link element for the trip
         const tripLink = document.createElement('a');
