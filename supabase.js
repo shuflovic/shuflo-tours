6e0291fcc9eaa30e7ce4d9d9aacad00560506954
shuflovic <pavel.skuta@gmail.com>
2025-04-26 21:08:12 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://rigsljqkzlnemypqjlbk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZ3NsanFremxuZW15cHFqbGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjI5NTUsImV4cCI6MjA2MTIzODk1NX0.hNdNu9fHGQfdh4WdMFx_SQAVjXvQutBIud3D5CkM9uY';
+
+// Initialize the Supabase client
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Export the client if using modules
+// export { supabaseClient };
