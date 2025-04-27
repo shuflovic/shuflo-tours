3de8ef6f80c6fa239b1f0df44dd7c594fa9895c9
shuflovic <pavel.skuta@gmail.com>
2025-04-27 15:30:18 +0700

    Update trip-list.js

--- trip-list.js
+++ trip-list.js
@@ -1,10 +1,7 @@
 const SUPABASE_URL = 'https://rigsljqkzlnemypqjlbk.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZ3NsanFremxuZW15cHFqbGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjI5NTUsImV4cCI6MjA2MTIzODk1NX0.hNdNu9fHGQfdh4WdMFx_SQAVjXvQutBIud3D5CkM9uY';
-
-// Initialize Supabase client
-// For browser environments, we need to use the global supabase object
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
+        
 // Function to fetch trips from Supabase and populate them in the "trip-list"
 async function populateTripList() {
     const tripListDiv = document.getElementById("trip-list");
