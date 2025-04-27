9858ee60142d03b7948e0d4ed489e0d7e1e826d3
shuflovic <pavel.skuta@gmail.com>
2025-04-27 08:53:55 +0700

    Update trip-list.js

--- trip-list.js
+++ trip-list.js
@@ -1,6 +1,7 @@
+const SUPABASE_URL = 'https://rigsljqkzlnemypqjlbk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZ3NsanFremxuZW15cHFqbGJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjI5NTUsImV4cCI6MjA2MTIzODk1NX0.hNdNu9fHGQfdh4WdMFx_SQAVjXvQutBIud3D5CkM9uY';
 
-const SUPABASE_URL = 'https://your-supabase-url.supabase.co';
-const SUPABASE_ANON_KEY = 'your-supabase-anon-key';
+
 
 // Initialize Supabase client
 const { createClient } = require('@supabase/supabase-js'); // Ensure you have the Supabase package installed
